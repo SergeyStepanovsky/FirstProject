b87386f9dd17bfc8dd75ee0fbac49b8ec990064c
SergeyStepanovsky <98726536+SergeyStepanovsky@users.noreply.github.com>
2022-11-09 09:44:27 +0800

    Add files via upload

---
+++ RestInterfaceDemo/src/main/java/FirstInterface/demo/FirstInterfaceDemoApplication.java
@@ -0,0 +1,13 @@
+package FirstInterface.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstInterfaceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstInterfaceDemoApplication.class, args);
+		System.out.println("Hello");
+	}
+}
---
+++ RestInterfaceDemo/src/test/java/FirstInterface/demo/FirstInterfaceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package FirstInterface.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstInterfaceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
